9bc68952a221c2197a345bb4bbb1bedeb78f60ba
epesco <epesco@wpi.edu>
2016-01-03 17:26:47 -0500

    about me

--- js/main-js.js
+++ js/main-js.js
@@ -1,11 +1,11 @@
 
 
-/* Fixing margin below absolute div (banner) */
-var profimgheight = $('#profileimg').height() + 5;
+/* Fixing margin below absolute div (banner)
+var profimgheight = $('#profileimg').height() -25 ;
 console.log(profimgheight)
 var heightwithpx = String(profimgheight) + "px";
 console.log(heightwithpx)
-$('#about').css('top', heightwithpx);
+$('#about').css('top', heightwithpx);*/
 
 
 var docwidth = jQuery(document).width()/2
