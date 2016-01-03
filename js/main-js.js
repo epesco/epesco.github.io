091ad603590865ac060f7c43e3684ff85f22aef0
epesco <epesco@wpi.edu>
2016-01-03 13:54:03 -0500

    about started

---
+++ js/main-js.js
@@ -0,0 +1,8 @@
+
+
+
+var profimgheight = $('#profileimg').height() + 5;
+console.log(profimgheight)
+var heightwithpx = String(profimgheight) + "px";
+console.log(heightwithpx)
+$('#about').css('top', heightwithpx);
