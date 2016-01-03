e3ce3f7cd75dd76bd0751de010c92a2842e22256
epesco <epesco@wpi.edu>
2016-01-03 14:53:28 -0500

    font

--- js/main-js.js
+++ js/main-js.js
@@ -6,9 +6,3 @@ console.log(profimgheight)
 var heightwithpx = String(profimgheight) + "px";
 console.log(heightwithpx)
 $('#about').css('top', heightwithpx);
-
-
-var docwidth = jQuery(document).width()
-var strdocwidth = String(docwidth) + "px";
-console.log(strdocwidth)
-$('#containerbanner').css('width', strdocwidth);
