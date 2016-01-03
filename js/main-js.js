f693dba89a79f2693822afac4248b59a14632f60
epesco <epesco@wpi.edu>
2016-01-03 15:38:52 -0500

    added header

--- js/main-js.js
+++ js/main-js.js
@@ -6,3 +6,9 @@ console.log(profimgheight)
 var heightwithpx = String(profimgheight) + "px";
 console.log(heightwithpx)
 $('#about').css('top', heightwithpx);
+
+
+var docwidth = jQuery(document).width()/2
+var strdocwidth = String(docwidth) + "px";
+console.log(strdocwidth)
+$('#mobileprofileimg').css('width', strdocwidth);
