aef5ee9721610ac32b06fd2509a47da9d550fd18
epesco <epesco@wpi.edu>
2016-01-03 14:11:13 -0500

    fixing iphone portrait size

--- js/main-js.js
+++ js/main-js.js
@@ -1,8 +1,13 @@
 
 
-
+/* Fixing margin below absolute div (banner) */
 var profimgheight = $('#profileimg').height() + 5;
 console.log(profimgheight)
 var heightwithpx = String(profimgheight) + "px";
 console.log(heightwithpx)
 $('#about').css('top', heightwithpx);
+
+
+var docwidth = jQuery(document).width()
+var strdocwidth = String(docwidth) + "px";
+$('#containerbanner').css('width', strdocwidth);
